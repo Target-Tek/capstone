91bc38c439f654db2d0e1e4c5df04ffd6474b031
Lukas Nordlin <lukas.nordlin@gmail.com>
2018-02-07 16:49:04 +0000

    Added keyboard interface code.

--- keyboard_read.py
+++ keyboard_read.py
@@ -1,4 +1,22 @@
 import curses
-stdscr = curses.initscr()
+from curses import wrapper
 
-print("I'm working!")
+def main(stdscr):
+	stdscr.clear()
+	print("Running some program")
+	while True:
+		# Store the key value in the variable 'direction'
+		direction = stdscr.getch()
+		# clear the terminal
+		stdscr.clear()
+		if direction == ord('6'):
+			stdscr.addstr("I want to move right")
+		elif direction == ord('8'):
+			stdscr.addstr("I want to move up")
+		elif direction == ord('4'):
+			stdscr.addstr("I want to move left")
+		elif direction == ord('2'):
+			stdscr.addstr("I want to move down")
+
+wrapper(main)
+
