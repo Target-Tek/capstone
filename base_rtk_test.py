ff712772732906eae763b3a971b638739bd3a548
Palmer Clegg <palmer.clegg91@gmail.com>
2018-02-09 09:42:16 -0700

    new messages added to base_rtk_test.  rover_rtk_test file created

--- base_rtk_test.py
+++ base_rtk_test.py
@@ -8,27 +8,33 @@ clrVTG = '$PUBX,40,VTG,0,0,0,0*5E\r\n'
 clrGSA = '$PUBX,40,GSA,0,0,0,0*4E\r\n'
 clrGSV = '$PUBX,40,GSV,0,0,0,0*59\r\n'
 
-CFGPRTbase = 'B5620600140001000000C0080000004B000000002000000000004ECD'
-CFGPRTbaseTEST = 'B5620600140001000000C0080000004B000000002000000000001122'
-ACKACKpoll = 'B562050100000617'
-ACKNAKpoll = 'B562050000000514'
+CFGTMODE3 = 'B5 62 06 71 28 00 00 00 02 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 2C 01 00 00 20 4E 00 00 00 00 00 00 00 00 00 00 3C 88'
+CFGPRTbase = 'B5 62 06 00 14 00 01 00 00 00 C0 08 00 00 00 4B 00 00 00 00 20 00 00 00 00 00 4E CD'
+MSG1005 = 'B5 62 06 01 08 00 F5 05 00 01 00 00 00 00 0A 73'
+MSG1077 = 'B5 62 06 01 08 00 F5 4D 00 01 00 00 00 00 52 6B'
+MSG1087 = 'B5 62 06 01 08 00 F5 57 00 01 00 00 00 00 5C B1'
+MSG1230 = 'B5 62 06 01 08 00 F5 E6 00 01 00 00 00 00 EB 9A'
+
+NAVSVINpoll = 'B5 62 01 3B 00 00 3C B5'
+NAVPVTpoll = 'B5 62 01 07 00 00 08 19'
 
 def read_msgs(ublox):
     i = 0
-    time.sleep(.5)
+    time.sleep(1)
     while(ublox.in_waiting > 0):
         print('# of bytes to be read:' + str(ublox.in_waiting))
         print('loop #' + str(i))
         i = i + 1
         print(ublox.readline())
-        #print('\n\r')
     return
 
 base_stn = serial.Serial('/dev/ttyACM0',9600)
-#print(CFGPRTbase.encode('utf-8'))
-CFGPRTbaseTEST = bytes.fromhex(CFGPRTbaseTEST)
-ACKACKpoll = bytes.fromhex(ACKACKpoll)
-#ACKNAKpoll = bytes.fromhex(ACKNAKpoll)
+#CFGTMODE3 = bytes.fromhex(CFGTMODE3) #step1: SVIN
+CFGPRTbase = bytes.fromhex(CFGPRTbase) #step2: RTCM3 out on UART1
+MSG1005 = bytes.fromhex(MSG1005) #step3: station coordinates
+MSG1077 = bytes.fromhex(MSG1077) #GPS coordinates
+MSG1087 = bytes.fromhex(MSG1087) #GLONASS observations
+MSG1230 = bytes.fromhex(MSG1230) #GLONASS code-phase biases
 read_msgs(base_stn)
 print('-----clearing default messages-----')
 #base_stn.write(clrGGA.encode('utf-8'))
@@ -39,11 +45,17 @@ base_stn.write(clrGSA.encode('utf-8'))
 base_stn.write(clrGSV.encode('utf-8'))
 print('-----reading leftover messages-----')
 read_msgs(base_stn)
-base_stn.write(CFGPRTbaseTEST)
-base_stn.write(ACKACKpoll)
-print('-----reading results of CFGPRT-----')
+print('-----writing RTK GPS enabling messages-----')
+#base_stn.write(CFGTMODE3)
+base_stn.write(CFGPRTbase)
+base_stn.write(MSG1005)
+base_stn.write(MSG1077)
+base_stn.write(MSG1087)
+base_stn.write(MSG1230)
+print('-----reading results of RTK GPS messages-----')
 read_msgs(base_stn)
-print('finished reading messages')
+#print(base_stn.read(base_stn.in_waiting))
+print('-----end of script-----')
 base_stn.close()
 
 
---
+++ rover_rtk_test.py
@@ -0,0 +1,44 @@
+import serial
+import time
+
+
+clrGGA = '$PUBX,40,GGA,0,0,0,0*5A\r\n'
+clrGLL = '$PUBX,40,GLL,0,0,0,0*5C\r\n'
+clrRMC = '$PUBX,40,RMC,0,0,0,0*47\r\n'
+clrVTG = '$PUBX,40,VTG,0,0,0,0*5E\r\n'
+clrGSA = '$PUBX,40,GSA,0,0,0,0*4E\r\n'
+clrGSV = '$PUBX,40,GSV,0,0,0,0*59\r\n'
+
+CFGPRTrover = 'B5 62 06 00 14 00 01 00 00 00 C0 08 00 00 00 4B 00 00 20 00 00 00 00 00 00 00 4E 0D'
+
+def read_msgs(ublox):
+    i = 0
+    time.sleep(1)
+    while(ublox.in_waiting > 0):
+        print('# of bytes to be read:' + str(ublox.in_waiting))
+        print('loop #' + str(i))
+        i = i + 1
+        print(ublox.readline())
+    return
+
+rover = serial.Serial('/dev/ttyACM0',9600)
+
+read_msgs(rover)
+print('-----clearing default messages-----')
+rover.write(clrGGA.encode('utf-8'))
+rover.write(clrGLL.encode('utf-8'))
+rover.write(clrRMC.encode('utf-8'))
+rover.write(clrVTG.encode('utf-8'))
+rover.write(clrGSA.encode('utf-8'))
+rover.write(clrGSV.encode('utf-8'))
+print('-----reading leftover messages-----')
+read_msgs(rover)
+print('-----writing RTK GPS enabling messages-----')
+rover.write(CFGPRTrover)
+print('-----reading results of RTK GPS messages-----')
+read_msgs(rover)
+#print(base_stn.read(rover.in_waiting))
+print('-----end of script-----')
+rover.close()
+
+
