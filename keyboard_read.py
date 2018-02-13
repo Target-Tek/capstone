ea2ffa72e8355e77c548de453ef3263a82adef04
Lukas Nordlin <lukas.nordlin@gmail.com>
2018-02-13 15:51:28 +0000

    Changed stepper frequency to a better number.

--- keyboard_read.py
+++ keyboard_read.py
@@ -4,6 +4,7 @@ from curses import wrapper
 from time import sleep
 import pigpio
 
+step_frequency = 2000	# Frequency of steps. You can change this.
 DIR = 20     	# Direction GPIO Pin
 STEP = 21	# Step GPIO Pin
 STEP2 = 26	# Step GPIO PIN second motor
@@ -34,34 +35,38 @@ def main(stdscr):
 			stdscr.addstr("No key press")
 			pi.set_PWM_dutycycle(STEP, 0)
 			pi.set_PWM_dutycycle(STEP2, 0)
+			pi.set_PWM_frequency(STEP, 0)
+			pi.set_PWM_frequency(STEP2, 0)
 
 		elif direction == ord('6'):
 			stdscr.addstr("I want to move right")
 			pi.write(DIR, True)
 			pi.set_PWM_dutycycle(STEP, 128)		# 50% On 50% Off
-			pi.set_PWM_frequency(STEP, 4000)	# 4000 pulses per second
+			pi.set_PWM_frequency(STEP, step_frequency)	# 4000 pulses per second
 
 		elif direction == ord('8'):
 			stdscr.addstr("I want to move up")
 			pi.write(DIR2, True)
 			pi.set_PWM_dutycycle(STEP2, 128)		# 50% On 50% Off
-			pi.set_PWM_frequency(STEP2, 4000)
+			pi.set_PWM_frequency(STEP2, step_frequency)
 		elif direction == ord('4'):
 			stdscr.addstr("I want to move left")
 			pi.write(DIR, False)
 			pi.set_PWM_dutycycle(STEP, 128)		# 50% On 50% Off
-			pi.set_PWM_frequency(STEP, 4000)	# 4000 pulses per second
+			pi.set_PWM_frequency(STEP, step_frequency)	# 4000 pulses per second
 
 		elif direction == ord('2'):
 			stdscr.addstr("I want to move down")
 			pi.write(DIR2, True)
 			pi.set_PWM_dutycycle(STEP2, 128)		# 50% On 50% Off
-			pi.set_PWM_frequency(STEP2, 4000)
+			pi.set_PWM_frequency(STEP2, step_frequency)
 		else:
 			stdscr.addstr("I dont'know that key")
 			direction = '0'
 			pi.set_PWM_dutycycle(STEP, 0)
 			pi.set_PWM_dutycycle(STEP2, 0)
+			pi.set_PWM_frequency(STEP, 0)
+			pi.set_PWM_frequency(STEP2, 0)
 
 wrapper(main)
 
