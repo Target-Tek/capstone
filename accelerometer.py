863659bdb2dd665332dfeddeb868beb5abd0f274
Lukas Nordlin <lukas.nordlin@gmail.com>
2018-03-06 15:59:57 +0000

    Set output data rate to the minimum, and included functions for computing angular offsets from raw data.

--- accelerometer.py
+++ accelerometer.py
@@ -11,27 +11,28 @@
 
 from time import sleep # Give us access to a clock and time.sleep(seconds).
 from statistics import mean # Take averages. Requires 3.4 or later.
-import spidev # Import py-spidev library for SPI communications.
+from spidev import * # Import py-spidev library for SPI communications.
 from numpy import int8 # For converting two's complement bytes to ints
+from math import pi # Used for converting from radians to degrees.
 
 class Accelerometer:
     # Constants for all class instances.
-    error_tolerance = 1	# Leeway in the accelerometer reading.
     number_of_readings = 8	# Take 8 readings at 45 degree increments.
     settling_time_delay_s = .500	# Wait this many ms for device to settle before taking a reading.
     time_to_rotate_s = 2.000	# Allow this long for gimbal to rotate before taking the next reading.
-    number_of_flashes = 10	# Number of blinks to indicate we're done. Purely visual.
-    flash_delay_s = .050	# Time between blinks. Purely visual.
+    lsb_per_g = 1024 # Least significant bits per g, used to interpret output.
 
     # Constructor. Initializes things to zero.
     def __init__(self):
         self.count = 0	# Initialize count. How many readings have we taken.
         self.x_golden, self.y_golden, self.z_golden = -1, -1, -1    # Initialize to error value.
         self.x_readings, self.y_readings, self.z_readings = [], [], []   # Initialize data vectors.
-        self.spi = spidev.SpiDev() # Create SPI object.
+        self.spi = SpiDev() # Create SPI object.
         self.spi.open(0,0) # Open SPI object for communication.
         self.spi.max_speed_hz = 100000 # Set communication rate
         self.spi.mode = 0b11 # Set CPOL/CPHA
+        self.spi.xfer([0x2C, 0x06]) # Set lowest output data rate for minimum noise.
+        self.spi.xfer([0x31, 0x80]) # Set full resolution mode, 1024 LSB/g.
         self.spi.xfer([0x2D, 0x08]) # Turn on accelerometer's measurement mode.
 
     def test(self):
@@ -75,15 +76,20 @@ class Accelerometer:
         output.append((int8(data[4]) << 8) + int8(data[3])) # Concatanate bytes
         return output
 
-    def x_offset(self):
-        [x_read, y_read] = self.raw_output() # Read accelerometer data.
-        x_read = x_read - self.x_golden
-        y_read = y_read - self.y_golden
-        
-
-    def y_offset(self):
-        pass # TODO: give difference from level in y direction
-
     def offset(self):
+        # Returns a vector containing angular offsets from level, in degrees.
+        [x_read, y_read] = self.raw_output() # Read accelerometer data.
+        x_read = x_read - self.x_golden # Find our error in LSBs.
+        y_read = y_read - self.y_golden # Find our error in LSBs.
+        x_offset = x_read / lsb_per_g # Convert our error to g's.
+        y_offset = y_read / lsb_per_g # Convert our error to g's.
+        # Small-angle approximation: The g's we are off by
+        # is equal to the radians we are off by.
+        x_offset = x_offset * 180 / pi # Convert to degrees.
+        y_offset = y_offset * 180 / pi # Convert to degrees.
+        return [x_offset, y_offset] # Angular offset from level in degrees
+
+    def offset_total(self):
         # Return overall angular error from level.
-        return float(x_offset(self)**2 + y_offset(self)**2)**0.5
+        [x_offset, y_offset] = self.offset() # Compute individual offsets.
+        return float(x_offset(self)**2 + y_offset(self)**2)**0.5 # Combine them.
