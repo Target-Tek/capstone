1452690542dbe90eda9eebeaf8300cf16ac6f64f
Palmer Clegg <palmer.clegg91@gmail.com>
2018-03-29 09:57:40 -0600

    Finalized code for ramping and keyboard read.

--- ramp.py
+++ ramp.py
@@ -4,9 +4,6 @@ from curses import wrapper
 from time import sleep
 #import pigpio
 import RPi.GPIO as GPIO
-from picamera import PiCamera
-from time import sleep
-
 
 #step_frequency = 8000	# Frequency of steps. You can change this.
 DIR = 20     	# Direction GPIO Pin first motor
@@ -14,7 +11,10 @@ STEP = 21	# Step GPIO Pin first motor
 STEP2 = 26	# Step GPIO PIN second motor
 DIR2 = 12	# Direction for second motor
 #ENABLE = 16	# GPIO pin for enable which should be set low for steppers to work
-delay = .000025
+delay1 = .0005
+delay2 = .00001
+delay3 = .000025
+
 cw = 0
 ccw = 1
 # Connect to pigpiod daemon
@@ -36,14 +36,13 @@ k = 1
 l = 1
 
 def main(stdscr):
-	steps =1000		#This is the initial step size of 1.8 degrees
+	steps = 5000		#This is the initial step size of 1.8 degrees
+	step1 = 1000
+	step2 = 2000
+	step3 = steps-step1-step2
 	stdscr.clear()
 	stdscr.nodelay(True)
 	print("Running some program")
-	camera = PiCamera()
-	camera.start_preview()
-	#sleep(10000)
-	#camera.stop_preview()
 	while True:
 		# Store the key value in the variable 'direction'
 		direction = stdscr.getch()
@@ -59,63 +58,55 @@ def main(stdscr):
 			#pi.set_PWM_frequency(STEP, 0)
 			#pi.set_PWM_frequency(STEP2, 0)
 			
-		elif direction == ord('6'):
-			stdscr.addstr("I want to move right")
+		elif direction == ord('5'):
+			stdscr.addstr("RAMPING")
 			#pi.write(DIR, True)
-			for x in range(steps):
+			for x in range(step1):
 				GPIO.output(DIR,ccw)
 				GPIO.output(STEP,GPIO.HIGH)
-				sleep(delay)
+				sleep(delay1)
 				GPIO.output(STEP,GPIO.LOW)
-				sleep(delay)
+				sleep(delay1)
+			for x in range(step2):
+				GPIO.output(DIR,ccw)
+				GPIO.output(STEP,GPIO.HIGH)
+				sleep(delay2)
+				GPIO.output(STEP,GPIO.LOW)
+				sleep(delay2)
+			for x in range(step3):
+				GPIO.output(DIR,ccw)
+				GPIO.output(STEP,GPIO.HIGH)
+				sleep(delay3)
+				GPIO.output(STEP,GPIO.LOW)
+				sleep(delay3)
 			#pi.set_PWM_dutycycle(STEP, 128)		# 50% On 50% Off
 			#pi.set_PWM_frequency(STEP, step_frequency)	# 4000 pulses per second
 				
-		elif direction == ord('8'):
-			stdscr.addstr("I want to move up")
-			#pi.write(DIR2, True)
-			for x in range(steps):
-				GPIO.output(DIR2,cw)
-				GPIO.output(STEP2,GPIO.HIGH)
-				sleep(delay)
-				GPIO.output(STEP2,GPIO.LOW)
-				sleep(delay)
-			#pi.set_PWM_dutycycle(STEP2, 128)		# 50% On 50% Off
-			#pi.set_PWM_frequency(STEP2, step_frequency)
 		elif direction == ord('4'):
-			stdscr.addstr("I want to move left")
-			#pi.write(DIR, False)
-			for x in range(steps):
+			stdscr.addstr("RAMPING")
+			#pi.write(DIR, True)
+			for x in range(step1):
 				GPIO.output(DIR,cw)
 				GPIO.output(STEP,GPIO.HIGH)
-				sleep(delay)
+				sleep(delay1)
 				GPIO.output(STEP,GPIO.LOW)
-				sleep(delay)
+				sleep(delay1)
+			for x in range(step2):
+				GPIO.output(DIR,cw)
+				GPIO.output(STEP,GPIO.HIGH)
+				sleep(delay2)
+				GPIO.output(STEP,GPIO.LOW)
+				sleep(delay2)
+			for x in range(step3):
+				GPIO.output(DIR,cw)
+				GPIO.output(STEP,GPIO.HIGH)
+				sleep(delay3)
+				GPIO.output(STEP,GPIO.LOW)
+				sleep(delay3)
 			#pi.set_PWM_dutycycle(STEP, 128)		# 50% On 50% Off
 			#pi.set_PWM_frequency(STEP, step_frequency)	# 4000 pulses per second
-		elif direction == ord('2'):
-			stdscr.addstr("I want to move down")
-			#pi.write(DIR2, True)
-			for x in range(steps):
-				GPIO.output(DIR2,ccw)
-				GPIO.output(STEP2,GPIO.HIGH)
-				sleep(delay)
-				GPIO.output(STEP2,GPIO.LOW)
-				sleep(delay)
-			#pi.set_PWM_dutycycle(STEP2, 128)		# 50% On 50% Off
-			#pi.set_PWM_frequency(STEP2, step_frequency)
-		elif direction == ord('9'):
-			stdscr.addstr("Large Step size")
-			steps = 1000	#This is a step size of .18 degrees
-		elif direction == ord('7'):
-			stdscr.addstr("Medium Step size")
-			steps = 100	#This is a step size of .18 degrees
-		elif direction == ord('3'):
-			stdscr.addstr("Small Step size")
-			steps = 10	#This is a step size of .018 degrees
-		elif direction == ord('1'):
-			stdscr.addstr("Tiny Step size")
-			steps = 1	#This is a step size of .0018 degrees
+				
+		
 		else:
 			stdscr.addstr("I dont'know that key")
 			#direction = '0'
